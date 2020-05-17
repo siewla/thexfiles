c78d4d9e7cc024257584e5ccad80c5fb0a377876
siewla <siewlapang@gmail.com>
2020-05-17 23:51:57 +0800

    modified to requirements

--- routes/sightings.js
+++ routes/sightings.js
@@ -1,44 +1,84 @@
+const QUERY_DELIMITER = '&';
+const KEY_VALUE_DELIMITER = '=';
+const VALID_QUERY_KEYS = ['shape', 'date', 'state', 'city'];
+
+
 //access the app data 
 const sightingsData = require('../sightings.json');
+
 
 const searchSightings = (data, key, query) =>{
     return data.filter(sighting => 
         sighting[key].toLowerCase()===query);
 };
 
+const isValidQueryKey = key =>VALID_QUERY_KEYS.includes(key);
+
+const mapQueryData = (queryObj, key, value) => {
+    if(isValidQueryKey(key)){
+        queryObj[key] = value;
+    }
+    return queryObj;
+};
+
+const buildQueriesObject = (queries) => {
+    const queriesSplit = queries.split(QUERY_DELIMITER);
+    const resultsQuery ={};
+
+    queriesSplit.forEach(query =>{
+        const [key, value] = query.split(KEY_VALUE_DELIMITER);
+        mapQueryData(resultsQuery, key, value);
+    });
+    return resultsQuery;
+};
+
+const findSightings = (queryString = '') => {
+    console.log(queryString);
+    const query = buildQueriesObject(queryString);
+    console.log(query);
+    return Object.keys(query)
+        .reduce((data, filterKey) => {
+            const filterValue = query[filterKey];
+            return searchSightings(data, filterKey, filterValue);
+        }, sightingsData);
+
+};
 
 
 module.exports = app => {
     app.get('/', (req,res) =>{
-        res.send('Welcome to The X-Files');
-    });
-    
-    app.get('/sightings', (req,res) =>{
         res.render('sightings/main');
     });
-
-    app.get('/sightings/results', (req,res) =>{
-        const state = req.query.state.toLowerCase();
-        const city = req.query.city.toLowerCase();
-        const shape = req.query.shape.toLowerCase();
-        // const results = multiSearch(sightingsData, req.query.state ,req.query.city, req.query.shape);
-
-        const results = sightingsData.filter((item) => {
-            return (item.state.toLowerCase() == state || state == '') &&
-                    (item.city.toLowerCase() == city || city == '') &&
-                    (item.shape.toLowerCase() == shape || shape == '');
-        });
-       
-        res.render('sightings/search', { results });
-    });
-
-    app.get('/sightings/:key/:query', (req,res) =>{
-        const results = searchSightings(sightingsData, req.params.key ,req.params.query);
-        res.render('sightings/search', { results });
-    });
     
-    //example
-    //http://localhost:3000/sightings/state/ca
-    //http://localhost:3000/sightings/city/lincoln
-    //http://localhost:3000/sightings/shape/sphere
+    app.get('/sightings/', (req,res) =>{
+        let queriesString = '';
+        let state='';
+        let city='';
+        let shape='';
+
+        if (req.query.state&&req.query.city&&req.query.shape){
+            state = req.query.state.toLowerCase();
+            city = req.query.city.toLowerCase();
+            shape = req.query.shape.toLowerCase();
+        }
+        
+        if (state!=='' && city!=='' && shape !==''){
+            queriesString= `state=${state}&city=${city}&shape=${shape}`;
+        } else if (state!==''&&city!==''&&shape===''){
+            queriesString= `state=${state}&city=${city}`;
+        } else if (state!==''&&city===''&&shape!==''){
+            queriesString= `state=${state}&shape=${shape}`;
+        } else if (state===''&&city!==''&&shape!==''){
+            queriesString= `shape=${shape}&city=${city}`;
+        } else if (state!==''&&city===''&&shape===''){
+            queriesString= `state=${state}`;
+        } else if (state===''&&city!==''&&shape===''){
+            queriesString= `city=${city}`;
+        } else if (state===''&&city===''&&shape!==''){
+            queriesString= `shape=${shape}`;
+        } 
+
+        const results = findSightings(queriesString);
+        res.render('sightings/search', { results });
+    });
 };
